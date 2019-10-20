371a9a974f53cd6e2c8c286735383458dfeb3a0d
Magva7 <magva7@gmail.com>
2019-10-20 16:58:34 +0400

    add build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+//
+
+        int arg1 = 0;
+        System.out.println(arg1);
     }
 }
