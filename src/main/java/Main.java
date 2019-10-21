abd8a8a12f288844bab50c0ffe3d13ce2b0850e6
Magva7 <magva7@gmail.com>
2019-10-21 22:51:16 +0400

    add info in pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
       System.out.println("Hello World!");
 
-//       int args[] = {3, 5, 7};
         System.out.println("Всего аргументов в командной строке: " + args.length);
+        System.out.println("Выводим аргумент 1" +args[0]);
+        System.out.println("Выводим аргумент 2" +args[1]);
     }
 }
