cf4f4eb4b6e6548a999f11163a8cf44010b954a7
Magva <magva7@gmail.com>
2019-10-24 01:07:38 +0300

    Удалил лишнее из кода, то, что все равно не работало, сейчас сделаю package и попробую смерджить с мастером

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,35 +2,27 @@ public class Main {
     public static void main(String[] args) {
 //      System.out.println("Hello World!");
 
+        /**Тест вывода аргументов*/
         System.out.println("Аргумент 1: " +args[0]);
         System.out.println("Аргумент 2: " +args[1]);
         System.out.println("Аргумент 3: " +args[2]);
 
-        /**Задания по практике*/
-        /**Сумма трех чисел*/
-        /**Сумма*/
-//        int sum = args[0] + args[1] + args[2]; показывает error, т.к. подаем тип string, а переменная int
-
-        /**Приводим в int */
-        int sum1 = Integer.valueOf(args[0] + args[1] + args[2]);
-        System.out.println("sum1 - сумма трех чисел равна: " +sum1);
-
-        /**Приводим в int */
-        int sum2 = Integer.valueOf(sum1);
-        System.out.println("sum2 - сумма трех чисел равна: " +sum2);
-
-        /**Среднее от 3 чисел - делим на 3 (Получив три числа, вывести среднее значение от их суммы)*/
-        float AverageSum =  sum1/ 3;
-        System.out.println("Сумма трех чисел, деленная на 3 равна: " +AverageSum);
-
-        /**Пробуем по одному аргументу*/
+        /**Переводим аргументы в int по одному*/
         int arg1 = Integer.valueOf(args[0]);
         int arg2 = Integer.valueOf(args[1]);
         int arg3 = Integer.valueOf(args[2]);
+
+        /**Суммируем 3 аргумента*/
         int argSum = arg1 + arg2 + arg3;
+
+        /**Делим сумму на 3*/
         int average = argSum/3;
-        System.out.println("argSum: " +argSum);
-        System.out.println("average: " +average);
+
+        /**Выводим для теста сумму трех аргументов*/
+        System.out.println("Сумма аргументов равна: " +argSum);
+
+        /**Выводим сумму, деленную на 3*/
+        System.out.println("Сумма аргументов, деленная на 3 равна: " +average);
 
         /**Произведение 3 чисел*/
 //        int umnoj = Integer.valueOf(args[0] * args[1] * args[2]);
