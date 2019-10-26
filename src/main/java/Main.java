f1d00b38c6a44eb7121bcdbecdb543d76756bc97
Magva7 <magva7@gmail.com>
2019-10-26 05:08:45 +0400

    Теперь переменные переводятся циклом и можно передавать сколько угодно аргументов, все просуммируется и поделится на количество

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,71 +1,42 @@
 public class Main {
 public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
 
-        /**Тест вывода аргументов*/
-        System.out.println("Аргумент 1: " + args[0]);
-        System.out.println("Аргумент 2: " + args[1]);
-        System.out.println("Аргумент 3: " + args[2]);
+//        /**Тест вывода аргументов*/
+//        System.out.println("Аргумент 1: " + args[0]);
+//        System.out.println("Аргумент 2: " + args[1]);
+//        System.out.println("Аргумент 3: " + args[2]);
+        /**Объявляем переменную, в которой будет храниться длина массива args, т.е. какое количество
+         * аргументов мы передали,чтобы потом мы знали, на сколько делить*/
+        int countArg = args.length;
 
+        /**Объявляем переменную, в которой будет храниться сумма введенных аргументов*/
+        int argSum = 0;
+
+        /**Набиваем переменную argSum - переводим принятые снаружи в терминале аргументы в int через цикл.
+         * Переводим каждый элемент, перебирая их по порядку, каждую итерацию в переменную argSum
+         * будет прибавляться аргумент, переведенный в int*/
+        for (int i = 0; i < args.length; i++) {
+                argSum += Integer.valueOf(args[i]);
+
+                /**Выводим для теста, как меняется переменная argSum каждую итерацию*/
+//                System.out.println("Аргументы argSum: " + argSum);
         }
 
-/**Суммирование аргументов*/
-//        /**Переводим аргументы в int по одному*/
-//        int arg1 = Integer.valueOf(args[0]);
-//        int arg2 = Integer.valueOf(args[1]);
-//        int arg3 = Integer.valueOf(args[2]);
-//
+        /**Вводим новую переменную у которой значение будет равно тому, что нам вернет метод average, в  который
+         * мы передаем 2 параметра, это наши перменные argSum и countArg, т.е. передаем сумму переменных
+         * и их количество, чтобы потом в методе поделилась сумма на количество*/
+        int result = average(argSum, countArg);
 
-/**
- * Тестируем метод
- */
-public static void test() {
-        int a = 15;
-        System.out.println(a);
+        /**Выводим сумму аргументов, деленную на их количество*/
+        System.out.println("Сумма аргументов, деленная на 3 равна: " +result);
         }
 
+ /** Объявляем новый метод, который принимает снаружи из метода main переменные, в argSum уже лежит сумма аргументов,
+  * в countArg их количество,а сам метод делит одну переменную на другую и возвращает отдает наружу результат*/
+public static int average(int argSum, int countArg) {
 
-/**
- * Объявляем новый метод 1, который принимает 3 аргумента
- */
-public static int average(String[] args) {
-        int arg1 = Integer.valueOf(args[0]);
-        int arg2 = Integer.valueOf(args[1]);
-        int arg3 = Integer.valueOf(args[2]);
-        int averageSum = (arg1 + arg2 + arg3) / 3;
-
-        System.out.println("1 Сумма чисел, делённая на 3 равна: " + averageSum);
-
-        return averageSum;
-        }
-
-/**
- * Объявляем новый метод 2, который принимает 3 аргумента
- */
-public static void average2(int arg4, int arg5, int arg6) {
-//        int arg4 = Integer.valueOf(arg4);
-//        int arg5 = Integer.valueOf(arg5);
-//        int arg6 = Integer.valueOf(arg6);
-        int averageSum2 = (arg4 + arg5 + arg6) / 3;
+        int average = argSum/countArg;
+        return average;
         }
 }
-//        System.out.println("2 Сумма чисел, делённая на 3 равна: " +averageSum2);
-//    }
-//
-//        /**Делим сумму на 3*/
-//        int average = argSum/3;
-//
-//        /**Выводим для теста сумму трех аргументов*/
-//        System.out.println("Сумма аргументов равна: " +argSum);
-//
-//        /**Выводим сумму, деленную на 3*/
-//        System.out.println("Сумма аргументов, деленная на 3 равна: " +average);
-
-//        /**Тест вывода переменной с циклом*/
-//      for (int i = 0; i < args.length; i++) {
-//          int varNew3 = Integer.valueOf(args[i]);
-//          int varNew4 = varNew3 + 10;
-//          System.out.println("Вывод переменной в цикле. arg[0] + 10 = " + varNew4);
-//      }
-
-//}
