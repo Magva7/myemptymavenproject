39e2b077b72d5cca6e8f0d063d47db7862bc708b
Magva <magva7@gmail.com>
2019-11-11 10:40:03 +0300

    Добавил ветку для Javarush,  с проверкой условия, какая переменная передана

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,48 +4,56 @@ public class User {
     short age;
     int height;
 
-    public User(String name, short age, int height) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
+    public User(String name, int a, int b) { /*Т.е. суть такая - передаем 3 переменных, стровую, это по-любому имя и две
+    числовых, а вот чтобы понять, какая из них возраст, а какая рост - делаем проверку, если число в диапазоне от 1 до 100,
+    то 99% что это возраст, а если больше, то 99% что это рост*/
+        this.name = name; //Имя
+
+        if (a > 0 && a < 100){ //Сравниваем, если переменная a у нас в диапазоне от 1 до 100
+            this.age = (short) a; //то это по-любому возраст
+            this.height = b; //а значит вторая переменная это рост
+
+        } else { //если не так, то значит наоборот
+            this.age = (short) b;
+            this.height = a;
+        }
     }
 
-    public User(String name, int height, short age) {
-        this.name = name;
-        this.height = height;
-        this.age = age;
-    }
 
-    public User(int height, String name, short age) {
-        this.height = height;
-        this.name = name;
-        this.age = age;
-    }
+    public User(int a, String name, int b) { /*Меняем перменные местами*/
 
-    public User(int height, short age, String name) {
-        this.height = height;
-        this.age = age;
+        if (a > 0 && a < 100){
+            this.age = (short) a;
+            this.height = b;
+
+        } else {
+            this.age = (short) b;
+            this.height = a;
+        }
+
         this.name = name;
     }
 
-    public User(short age, String name, int height) {
-        this.age = age;
-        this.name = name;
-        this.height = height;
-    }
+    public User(int a, int b, String name) { /*Меняем перменные местами*/
 
-    public User(short age, int height, String name) {
-        this.age = age;
-        this.height = height;
+        if (a > 0 && a < 100){
+            this.age = (short) a;
+            this.height = b;
+
+        } else {
+            this.age = (short) b;
+            this.height = a;
+        }
+
         this.name = name;
     }
 
     public static void main(String[] args) {
         /*Для теста создаем разные объекты и передаем параметры в разном порядке, чтобы протестить, что
         наши конструкторы работают*/
-        User Vasya = new User("Вася", (short) 11, 178); /*надо переводить  второй параметр
+        User Vasya = new User("Вася", 11, 178); /*надо переводить  второй параметр
         в  short, а то воспринимает как int*/
-        User Petya = new User("Петя", 190, (short) 17);/*надо переводить  третий параметр
+        User Petya = new User("Петя", 190, 17);/*надо переводить  третий параметр
         в  short, а то воспринимает как int*/
         User Olya = new User((short) 21, "Оля", 176);/*надо переводить  первый параметр
         в  short, а то воспринимает как int*/
